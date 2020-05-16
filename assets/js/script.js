d791f434702eafacfe1ee6f10e5b42fc92cac256
rosidqumaidi <rosidqumaidi46@gmail.com>
2020-05-16 16:55:35 +0700

    semantic web dicoding

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+
+container.addEventListener('click', function(e){
+
+    if( e.target.className == 'thumb' ) {
+        jumbo.src = e.target.src;
+        
+    }
+});
